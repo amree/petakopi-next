43446f0e4663fadcfd0714ecce8365520b03f4bd
Amree Zaid <mohd.amree@gmail.com>
2024-10-04 23:11:29 +0800

    Fix type issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
+interface CoffeeShop {
+  uuid: string;
+  name: string;
+}
+
 export default async function Home() {
   const data = await fetch("https://petakopi.my/api/v1/coffee_shops");
-  const shops = await data.json();
+  const coffeeShops = await data.json();
 
   return (
     <ul>
-      {shops.data.coffee_shops.map((shop) => (
-        <li key={shop.uuid}>
-          <h2>{shop.name}</h2>
+      {coffeeShops.data.coffee_shops.map((coffeeShop: CoffeeShop) => (
+        <li key={coffeeShop.uuid}>
+          <h2>{coffeeShop.name}</h2>
         </li>
       ))}
     </ul>
